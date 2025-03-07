a31cd4047ef8ee85bea51406f643d6ba83c77de5
vivek8091 <72376007+vivek8091@users.noreply.github.com>
2025-03-07 11:02:12 +0530

    Change password page completed

--- src/Components/ChangePassword.js
+++ src/Components/ChangePassword.js
@@ -1,9 +1,48 @@
-function ChangePassword(){
-    return(
-        <>
+import "../Styles/login.css";
+// import { Link } from "react-router-dom";
+
+function ChangePassword() {
+  return (
+    <>
+      <div className="change-pass">
+        <div className="pass-title mb-5">
+          <div className="pass-logo">
+            <i class="fa-solid fa-key"></i>
+          </div>
+          <h5>Change Password</h5>
+        </div>
+
+        <form>
+          <div class="mb-3">
+            <label for="currentPassword" class="form-label">
+              Current Password
+            </label>
+            <input
+              type="password"
+              class="form-control"
+              id="currentPassword"
+              aria-describedby="currentPass"
+            />
+          </div>
+          <div class="mb-3">
+            <label for="newPassword" class="form-label">
+              New Password
+            </label>
+            <input
+              type="password"
+              class="form-control"
+              id="newPassword"
+              aria-describedby="newPass"
+            />
+          </div>
+
+          <button type="submit" class="btn btn-primary w-100">
             Change Password
-        </>
-    );
+          </button>
+        </form>
+      </div>
+    </>
+  );
 }
 
 export default ChangePassword;
--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,11 +1,23 @@
+import "../Styles/contact.css";
+import { Link } from "react-router-dom";
 
 function Contact() {
-    return(
-        <>
-            {/* <Header /> */}
-            <h1>Contact</h1>
-        </>
-    );
+  return (
+    <>
+      <div className="events-harmoni">
+        <div className="content">
+            <h4>Contact Us Now</h4>
+          <h1>Keep in touch</h1>
+        </div>
+
+        <nav>
+          <Link to="/">Home</Link>
+          <span>|</span>
+          <Link to="/Contact">Contact</Link>
+        </nav>
+      </div>
+    </>
+  );
 }
 
 export default Contact;

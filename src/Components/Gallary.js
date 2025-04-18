6f169174bb6c274aa38f15bde78bc03ebe046ea5
vivek8091 <72376007+vivek8091@users.noreply.github.com>
2025-04-18 12:35:14 +0530

    Completed fetch API for gallary page...

--- src/Components/Gallary.js
+++ src/Components/Gallary.js
@@ -1,16 +1,27 @@
 import { Link } from "react-router-dom";
 import "../Styles/gallary.css";
-
-const gallaryImages = {
-  gallary1: require("../assets/gallary/gallary1.jpg"),
-  gallary2: require("../assets/gallary/gallary2.jpg"),
-  gallary3: require("../assets/gallary/gallary3.jpg"),
-  gallary4: require("../assets/gallary/gallary4.jpg"),
-  gallary5: require("../assets/gallary/gallary5.jpg"),
-  gallary6: require("../assets/gallary/gallary6.jpg"),
-};
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function Gallary() {
+  const [gallaryData, setGallaryData] = useState([]);
+  useEffect(() => {
+    const fetchGallaryData = async () => {
+      try {
+        const response = await axios.get(
+          "http://localhost:2121/api/gallary/getGallaryData/"
+        );
+        setGallaryData(response.data.data);
+        console.log(response.data);
+        // alert("Gallary data fetched successfully...");
+      } catch (error) {
+        console.error("Error while fetching gallary data!!!", error);
+        // alert("Could not fetch data!!!");
+      }
+    };
+
+    fetchGallaryData();
+  }, []);
   return (
     <>
       <div className="gallary-title">
@@ -27,26 +38,24 @@ function Gallary() {
 
       <div className="gallary-data container mt-3">
         <div className="row">
-          {Object.values(gallaryImages).map((image, index) => (
+          {gallaryData.map((image, index) => (
             <div key={index} className="col-md-6 col-sm-6 mb-3">
               <div className="image-container">
                 {/* Image */}
                 <img
-                  src={image}
-                  alt={`Gallery ${index + 1}`}
+                  src={`http://localhost:2121/uploads/${image.gallary_image}`}
+                  alt={image.gallary_image}
                   className="img-fluid"
                 />
 
                 {/* Overlay Content */}
                 <div className="img-details">
                   <div className="gallary-content">
-                    <h6>Hii there</h6>
+                    <h6>{image.gallary_title}</h6>
                     <p>Shello Here...</p>
                   </div>
                   <div className="gallary-icon">
-                    <a href="/">
-                      <i className="fa-solid fa-circle-info"></i>
-                    </a>
+                    <i className="fa-solid fa-circle-info"></i>
                   </div>
                 </div>
               </div>
@@ -57,13 +66,14 @@ function Gallary() {
 
       <div className="map mt-3 mb-3 container">
         <iframe
+          sandbox="allow-scripts allow-same-origin"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3671.631685220302!2d72.50929727530313!3d23.03729187916351!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x395e9b4922484c6f%3A0xe077cfffcd90ee87!2sSparks%20To%20Ideas%20Provide%20Final%20Year%20Project%20internship%20in%20Php%2CFlutter%2CPython%2CAI%2FML%2CReact%20JS%2CNode%20JS%2CMERN%2CCyber%20Security!5e0!3m2!1sen!2sin!4v1740725458908!5m2!1sen!2sin"
           title="Google Map"
           width="100%"
           height="300"
-          allowfullscreen=""
+          allowFullScreen
           loading="lazy"
-          referrerpolicy="no-referrer-when-downgrade"
+          referrerPolicy="no-referrer-when-downgrade"
         ></iframe>
       </div>
 
@@ -77,7 +87,9 @@ function Gallary() {
       </div>
 
       <div className="early-access">
-        <h1>Do you want to step into the <br /> future of your upcomming event?</h1>
+        <h1>
+          Do you want to step into the <br /> future of your upcomming event?
+        </h1>
         <button>Request Early Access</button>
       </div>
     </>
